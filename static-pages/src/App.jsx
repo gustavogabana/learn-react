f7f8bad493787787c67114c1eab53f05f69e4834
gustavogabana <gustavo_97@protonmail.com>
2025-07-23 17:28:34 -0300

    seo and semantics minor adjusts

--- static-pages/src/App.jsx
+++ static-pages/src/App.jsx
@@ -2,21 +2,21 @@ import './App.css'
 
 function TemporaryName() {
     return (
-      <main>
+      <>
         <header>
           <img src="./assets/reac-logo.png" alt="React logo" width="40px" />
         </header>
-        <h1>
-          Reasons why I'm excited to learn React
-        </h1>
-        <ol>
-          <li>React is a popular library, so I will be able to fit in with all the coolest devs out there!</li>
-          <li>I am more likely to get a job as a front end developer if I know React.</li>
-        </ol>
+        <main>
+          <h1>Reasons why I'm excited to learn React</h1>
+          <ol>
+            <li>React is a popular library, so I will be able to fit in with all the coolest devs out there!</li>
+            <li>I am more likely to get a job as a front end developer if I know React.</li>
+          </ol>
+        </main>
         <footer>
-          © 2025 Maximize development. All rights reserved.
+          <small>© 2025 Maximize development. All rights reserved.</small>
         </footer>
-      </main>
+      </>
     )
 }
 
