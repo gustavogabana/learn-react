e5ae04efb81debf7702a1e71d0abf1ef9e566e41
gustavogabana <gustavo_97@protonmail.com>
2025-07-23 17:41:10 -0300

    custom component challenge: parent/child components

--- static-pages/src/App.jsx
+++ static-pages/src/App.jsx
@@ -1,28 +1,46 @@
 import './App.css'
 
-function TemporaryName() {
+function Header() {
+  return (
+    <header>
+      <img src="./assets/reac-logo.png" alt="React logo" width="40px" />
+    </header>
+  )
+}
+
+function MainContent() {
+  return (
+    <main>
+      <h1>Reasons why I'm excited to learn React</h1>
+      <ol>
+        <li>React is a popular library, so I will be able to fit in with all the coolest devs out there!</li>
+        <li>I am more likely to get a job as a front end developer if I know React.</li>
+      </ol>
+    </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2025 Maximize development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function Page() {
     return (
       <>
-        <header>
-          <img src="./assets/reac-logo.png" alt="React logo" width="40px" />
-        </header>
-        <main>
-          <h1>Reasons why I'm excited to learn React</h1>
-          <ol>
-            <li>React is a popular library, so I will be able to fit in with all the coolest devs out there!</li>
-            <li>I am more likely to get a job as a front end developer if I know React.</li>
-          </ol>
-        </main>
-        <footer>
-          <small>© 2025 Maximize development. All rights reserved.</small>
-        </footer>
+        <Header />
+        <MainContent />
+        <Footer />
       </>
     )
 }
 
 function App() {
   return (
-    <TemporaryName/>
+    <Page />
   )
 }
 
