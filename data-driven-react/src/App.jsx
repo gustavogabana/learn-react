c84892692b08f085b75ff6cf542b2bf50875c8c3
gustavogabana <gustavo_97@protonmail.com>
2025-07-25 17:02:57 -0300

    travel journal challenge: entry component

--- data-driven-react/src/App.jsx
+++ data-driven-react/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Entry from './components/Entry'
 import Header from './components/Header'
 
 function App() {
   return (
     <>
       <Header />
+      <main className="container">
+        <Entry />
+      </main>
     </>
   )
 }
