8d536609a4373cfab9ccb90440af6710dfa715ae
gustavogabana <gustavo_97@protonmail.com>
2025-07-30 18:16:31 -0300

    chef claude: use state

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react"
+
 export default function Main() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = useState([])
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
@@ -9,8 +11,7 @@ export default function Main() {
         event.preventDefault()
         const formData = new FormData(event.currentTarget)
         const newIngredient = formData.get("ingredient")
-        ingredients.push(newIngredient)
-        console.log("ingredients: ", ingredients)
+        setIngredients((prev) => [...prev, newIngredient])
     }
 
     return (
