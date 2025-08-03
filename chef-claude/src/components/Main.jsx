82ecbe5cb4020fbf30c3812d395897d03cba68cb
gustavogabana <gustavo_97@protonmail.com>
2025-08-03 14:31:25 -0300

    chef claude: ingredients list and button conditional rendering

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -29,13 +29,14 @@ export default function Main() {
                 <ul className="ingredients-list" aria-live="polite">
                     {ingredientsListItems}
                 </ul>
+                {ingredients.length >= 4 && 
                 <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
+                </div>}
             </section>}
         </main>
     )
