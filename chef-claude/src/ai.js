22dfad0c0fb848db715e2139ae8c317a7e8e6572
gustavogabana <gustavo_97@protonmail.com>
2025-08-06 21:02:15 -0300

    chef claude: update huggingface import

--- chef-claude/src/ai.js
+++ chef-claude/src/ai.js
@@ -1,9 +1,9 @@
-import { HfInference } from '@huggingface/inference'
+import { InferenceClient } from '@huggingface/inference'
 
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `
-const hf = new HfInference(import.meta.env.HF_ACCESS_TOKEN)
+const hf = new InferenceClient(import.meta.env.HF_ACCESS_TOKEN)
 
 export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
@@ -16,7 +16,7 @@ export async function getRecipeFromMistral(ingredientsArr) {
             ],
             max_tokens: 1024,
         })
-        return response.choices[0].message.content
+        return response.choices[0].message
     } catch (err) {
         console.error(err.message)
     }
